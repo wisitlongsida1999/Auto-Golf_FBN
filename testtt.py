31ea9494204819f28be40d346587eb3205792dce
Wisit Longsida <wisitlongsida1999@gmail.com>
2022-10-20 10:00:18 +0700

    justCommit

--- testtt.py
+++ testtt.py
@@ -1,4 +1,28 @@
-i  = 'arissara_golf'
+import configparser
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from time import sleep
+import pandas as pd
+import sys
+import traceback
 
 
-print(type(i.split('_')[0]))
+
+
+
+
+
+# config
+my_config_parser = configparser.ConfigParser()
+my_config_parser.read('config.ini')
+payload = {
+    'username': my_config_parser.get('GOLF_LOGIN','yanee_id'),
+    'password': my_config_parser.get('GOLF_LOGIN','yanee_pwd'),
+
+    }
+
+
+username = payload['username']
+password = payload['password']
